4a5e8cd58bbe85aa5c0fc7c3b1d4c6daa40d9023
Nuno Guimaraes <nuno.guimaraes01@sap.com>
2024-10-05 01:28:10 +0100

    config

--- components/run.sh
+++ components/run.sh
@@ -10,5 +10,12 @@ set +a
 # SETUP POSTGRESQL OPERATOR             #
 #########################################
 
-kubectl apply -k postgres-operator/namespace
-kubectl apply --server-side -k postgres-operator/default
+# kubectl apply -k postgres-operator/namespace
+# kubectl apply --server-side -k postgres-operator/default
+
+#########################################
+# SETUP ARGO CD                         #
+#########################################
+
+# kubectl create namespace argocd
+# kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
